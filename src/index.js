cf409d1deed20cab2aa10603c5fc541f00dd7829
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2022-05-08 23:00:30 -0500

    new project location

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { app } from "./app/index.js";
+import { config } from "./config/index.js";
+
+const server = app.listen(process.env.PORT || config.port, () => {
+  console.log("server running");
+});
